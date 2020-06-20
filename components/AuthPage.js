756d826a1570e582f05a707bedd13605be203eae
svs987 <steve.voidstar@hotmail.co.uk>
2020-06-20 10:05:32 +0100

    Improved layout of page

--- components/AuthPage.js
+++ components/AuthPage.js
@@ -16,29 +16,37 @@ const AuthPage = ({ navigation }) => {
     console.log('isAuthorised: ', isAuthorised.state.authenticated);
 
     const handleValidation = async () => {
-        res = await handleAuthCode(authorisationCode);
-        console.log('return from handleAuthCode', res);
-        if (res) {
-            dispatch({ type: 'SET_AUTHENTICATED' });
-            if (isAuthorised.state.termsaccepted == 'ACCEPTED') {
-                dispatch({ type: 'SET_AUTH_PAGE_COMPLETED' }); //We can move on to the main app
+        try {
+            const res = await handleAuthCode(authorisationCode);
+            console.log('return from handleAuthCode', res);
+            if (res) {
+                dispatch({ type: 'SET_AUTHENTICATED' });
+                if (isAuthorised.state.termsaccepted == 'ACCEPTED') {
+                    dispatch({ type: 'SET_AUTH_PAGE_COMPLETED' }); //We can move on to the main app
+                } else {
+                    dispatch({ type: 'SET_TERMS_NOT_ACCEPTED' }); // Show that the terms and conditions need to be accepted
+                }
             } else {
-                dispatch({type: 'SET_TERMS_NOT_ACCEPTED'}); // Show that the terms and conditions need to be accepted
+                dispatch({ type: 'VALIDATION_FAILED' });
             }
-        } else {
-            dispatch({ type: 'VALIDATION_FAILED' });
-        }
+        } catch {
+            console.log('Error in handleValidation');
+        };
     }
 
     return (
-        <View>
-            <Text>Welcome to Sailing Buddy</Text>
-            <Text>Please enter the authorisation code sent to you by your yacht club</Text>
-            <TextInput
-                placeholder="Authorisation code"
-                style={styles.inputbox}
-                onChangeText={text => onChangeAuthorisationCode(text)}
-            />
+        <View style={styles.body}>
+            <View style={styles.titleBox}>
+                <Text style={styles.title}>Welcome to Sailing Buddy</Text>
+            </View>
+            <View>
+                <Text>Please enter the authorisation code sent to you by your yacht club</Text>
+                <TextInput
+                    placeholder="Authorisation code"
+                    style={styles.inputbox}
+                    onChangeText={text => onChangeAuthorisationCode(text)}
+                />
+            </View>
             {isAuthorised.state.authenticated == 'VALIDATION_FAILED' &&
                 <View>
                     <Text style={styles.authorisationError}>Error validating authorisation code. </Text>
@@ -46,23 +54,25 @@ const AuthPage = ({ navigation }) => {
                     <Text style={styles.authorisationError}>If you continue to have problems please contact your Yacht Club</Text>
                 </View>
             }
-            <Text>Please review the terms and conditions</Text>
-            <Button onPress={() => {
-                console.log('T&C button pressed');
-                return navigation.navigate('TandC')
-            }} title="Terms and Conditions" />
+            <View>
+                <Text>Please review the terms and conditions</Text>
+                <Button onPress={() => {
+                    console.log('T&C button pressed');
+                    return navigation.navigate('TandC')
+                }} title="Terms and Conditions" />
 
-            <CheckBox
-                title='I have reviewed and accept the Terms and Conditions for this App'
-                checked={isAuthorised.state.termsaccepted == 'ACCEPTED'}
-                onPress={() => dispatch({ type: 'TOGGLE_TERMS_ACCEPTED' })}
+                <CheckBox
+                    title='I have reviewed and accept the Terms and Conditions for this App'
+                    checked={isAuthorised.state.termsaccepted == 'ACCEPTED'}
+                    onPress={() => dispatch({ type: 'TOGGLE_TERMS_ACCEPTED' })}
 
-            />
-            {isAuthorised.state.termsaccepted == 'NOT_ACCEPTED_FAIL' &&
-                <View>
-                    <Text style={styles.authorisationError}>Please check the box to confirm that you have read and accept the terms and conditions</Text>
-                </View>
-            }
+                />
+                {isAuthorised.state.termsaccepted == 'NOT_ACCEPTED_FAIL' &&
+                    <View>
+                        <Text style={styles.authorisationError}>Please check the box to confirm that you have read and accept the terms and conditions</Text>
+                    </View>
+                }
+            </View>
             <Button onPress={() => { handleValidation() }} title="Validate Code" />
         </View>
 
@@ -73,11 +83,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    titleBox: {
+        paddingBottom: 20,
+    },
     body: {
         flex: 1,
-        paddingTop: 100,
         paddingHorizontal: 20,
+        justifyContent: 'space-around',
 
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
     },
     inputbox: {
         height: 40,
