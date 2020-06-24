55edfb4daf386e8e0413f0c91bb26075134386c0
svs987 <steve.voidstar@hotmail.co.uk>
2020-06-24 20:15:37 +0100

    Only get new access token when previous expires

--- components/logic/getAccessToken.js
+++ components/logic/getAccessToken.js
@@ -1,23 +1,32 @@
 import Constants from 'expo-constants';
 
+var accessData = null;
+
+const isExpired = () => {
+    return (new Date).getTime() > accessData.expireTime;
+}
+
 const getAccessToken = async () => {
     console.log('in getAccessToken. ');
-    const url = Constants.manifest.extra.getAccessTokenUrl;
+    if (!accessData || isExpired()) {
+        const url = Constants.manifest.extra.getAccessTokenUrl;
 
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: Constants.manifest.extra.getAccessTokenBody,
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: Constants.manifest.extra.getAccessTokenBody,
 
-    };
-     console.log('in getAccessToken. url is: ', url);
-    console.log('requestOptions are: ', requestOptions);
-    const accessResponse = await fetch(url, requestOptions);
-    console.log("Turning access token to json... ");
-    const accessData = await accessResponse.json();
-    console.log('Access data retrieved');
+        };
+        console.log('in getAccessToken. url is: ', url);
+        console.log('requestOptions are: ', requestOptions);
+        const accessResponse = await fetch(url, requestOptions);
+        console.log("Turning access token to json... ");
+        accessData = await accessResponse.json();
+        console.log('Access data retrieved: ', accessData);
+        accessData.expireTime = (new Date).getTime() + accessData.expires_in*900; //Ensure we ask for the token again before it expires
+    }
 
     return accessData.access_token;
 };
