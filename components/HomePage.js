df88fd22b4a10f03ddf4ee659b398011cc0bc4a6
svs987 <steve.voidstar@hotmail.co.uk>
2020-07-06 07:23:48 +0100

    Cleaned up spacing for Android

--- components/HomePage.js
+++ components/HomePage.js
@@ -5,46 +5,49 @@ import { Button } from 'react-native';
 /**
  * 
  */
-const HomePage = ({navigation}) => {
+const HomePage = ({ navigation }) => {
 
 	React.useLayoutEffect(() => {
 		navigation.setOptions({
-		  headerRight: () => (
-			<Button onPress={() => navigation.navigate('Info')} title="Info" />
-		  ),
+			headerRight: () => (
+				<Button onPress={() => navigation.navigate('Info')} title="Info" />
+			),
 		});
-	  }, [navigation]);
-	
+	}, [navigation]);
+
 
 	return (
 		<View style={styles.container}>
-				<View style={styles.body}>
-					<Text>If you're a skipper and you want to invite crew to sail with you, press Skipper</Text>
-					<Button onPress={() => navigation.navigate('MainSkipper')} title="Skipper" />  	    	  
-						<Text>If you're a crew and you want to find a boat to sail on, press Crew</Text>
-						<Button onPress={() => navigation.navigate('ViewInvites')} title="Crew" />  	    	  
-				</View>
+			<View style={styles.body}>
+				<Text style={styles.text}>If you're a skipper and you want to invite crew to sail with you, press Skipper</Text>
+				<Button onPress={() => navigation.navigate('MainSkipper')} title="Skipper" />
+				<Text style={styles.text}>If you're a crew and you want to find a boat to sail on, press Crew</Text>
+				<Button onPress={() => navigation.navigate('ViewInvites')} title="Crew" />
+			</View>
 		</View>
 	)
 
 }
 
 const styles = StyleSheet.create({
-	  container: {
+	container: {
 		flex: 1,
 		paddingHorizontal: 20,
-	  },
-	  body: {
-		 flex: 1,
-		 paddingTop: 100,
-		 paddingHorizontal: 20,
-	  
-	  },
-	  inputbox: { 
-		  height: 40, 
-		  borderColor: 'gray', 
-		  borderWidth: 1 
-		  },
-	});
+	},
+	body: {
+		flex: 1,
+		paddingTop: 100,
+		paddingHorizontal: 20,
+
+	},
+	inputbox: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1
+	},
+	text: {
+		paddingVertical: 5,
+	},
+});
 
 export { HomePage };
--- components/InfoPage.js
+++ components/InfoPage.js
@@ -5,38 +5,42 @@ import { Button } from 'react-native';
 /**
  * 
  */
-const InfoPage = ({navigation}) => {
+const InfoPage = ({ navigation }) => {
 
 
 	return (
 		<View style={styles.container}>
-				<View style={styles.body}>
-					<Text>View our privacy policy</Text>
-					<Button onPress={() => navigation.navigate('Privacy')} title="Privacy Policy" />  	    	  
-						<Text>View our terms and conditions</Text>
-						<Button onPress={() => navigation.navigate('TandC')} title="Terms and Conditions" />  	    	  
-				</View>
+			<View style={styles.body}>
+				<Text style={styles.text}>View our privacy policy</Text>
+				<Button onPress={() => navigation.navigate('Privacy')} title="Privacy Policy" />
+				<Text style={styles.text}>View our terms and conditions</Text>
+				<Button onPress={() => navigation.navigate('TandC')} title="Terms and Conditions" />
+			</View>
 		</View>
 	)
 
 }
 
 const styles = StyleSheet.create({
-	  container: {
+	container: {
 		flex: 1,
 		paddingHorizontal: 20,
-	  },
-	  body: {
-		 flex: 1,
-		 paddingTop: 100,
-		 paddingHorizontal: 20,
-	  
-	  },
-	  inputbox: { 
-		  height: 40, 
-		  borderColor: 'gray', 
-		  borderWidth: 1 
-		  },
-	});
+	},
+	body: {
+		flex: 1,
+		paddingTop: 100,
+		paddingHorizontal: 20,
+
+	},
+	inputbox: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1
+	},
+	text: {
+		paddingVertical: 5,
+	},
+
+});
 
 export { InfoPage };
